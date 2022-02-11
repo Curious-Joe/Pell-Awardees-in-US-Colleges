95b246ccb0d6f3a29e8e101d58a8bb4d531677ab
Curious Joe <a.h.fahad90@gmail.com>
2022-02-10 22:55:45 -0600

    added Heroku file, requirements and runtime

--- app_pell.py
+++ app_pell.py
@@ -3,9 +3,11 @@ import pathlib
 import re
 
 import dash
-import dash_core_components as dcc
+# import dash_core_components as dcc
+from dash import dcc
 import dash_bootstrap_components as dbc
-import dash_html_components as html
+# import dash_html_components as html
+from dash import html
 import pandas as pd
 import numpy as np
 from dash.dependencies import Input, Output, State
